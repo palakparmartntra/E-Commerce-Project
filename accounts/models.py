743e4345589ac75a2dc4edfa8b163c5c618d53f4
Yshah112 <yshah112002@gmail.com>
2024-01-19 12:00:36 +0530

    resloved changes in models

--- accounts/models.py
+++ accounts/models.py
@@ -3,27 +3,31 @@ from django.contrib.auth.models import AbstractUser
 from django.core.validators import MaxLengthValidator, MinLengthValidator
 # Create your models here.
 
+''' User model'''
+
 
 class User(AbstractUser):
     first_name = models.CharField(max_length=25)
     last_name = models.CharField(max_length=25)
-    phone_no = models.IntegerField(validators=[MinLengthValidator(10),MaxLengthValidator(10)])
+    phone_no = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         self.username
 
 
+''' Address model '''
+
 class Address(models.Model):
     reciever_name = models.CharField(max_length=25)
-    phone_no = models.IntegerField(validators=[MinLengthValidator(10), MaxLengthValidator(10)])
+    phone_no = models.IntegerField()
     house_no = models.IntegerField()
     street = models.CharField(max_length=150)
     is_primary = models.BooleanField(default=False)
     landmark = models.CharField(max_length=60)
     city = models.CharField(max_length=20)
     state = models.CharField(max_length=20)
-    zipcode = models.IntegerField(validators=[MinLengthValidator(6), MaxLengthValidator(6)])
+    zipcode = models.IntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='address')
 
 
