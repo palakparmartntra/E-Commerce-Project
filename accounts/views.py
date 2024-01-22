a49ded6fb0aa40ab98a6c221f3b0f7c73cfeccb3
Yshah112 <yshah112002@gmail.com>
2024-01-22 18:18:52 +0530

    delete update_profile

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render, redirect
-from allauth.account.forms import LoginForm, ResetPasswordForm
-from allauth.account.views import LogoutView
-from django.views.generic import UpdateView
-from .models import *
-from .forms import *
+from django.urls import reverse_lazy
+from .models import User
 
 
-# Create your views here.
 def homepage(request):
     return render(request, 'index.html')
 
 def my_account(request):
     return render(request, 'my-account.html')
+
+
+
+
+
+
+
+
+
 
