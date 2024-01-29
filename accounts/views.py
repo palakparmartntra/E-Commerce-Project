1baf8daf7c928ab18d6f9d79712dcbc2cf732ccb
Yshah112 <yshah112002@gmail.com>
2024-01-29 18:40:39 +0530

    changes:validation added login required, changre redirect page of updateprofile

--- accounts/views.py
+++ accounts/views.py
@@ -5,38 +5,46 @@ from .models import User, Address
 from .forms import AddressForm, UserUpdateForm
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
-
+from django.contrib.auth.decorators import login_required
 from django.views.generic.detail import DetailView
 from .models import User
 
 
 # Create your views here.
 
-
+@login_required(login_url='/login')
 class UpdateAddressView(UpdateView):
     """view for the update user address"""
 
     model = Address
     form_class = AddressForm
     template_name = 'update_address.html'
-    success_url = reverse_lazy('index')
+
+    def get_success_url(self):
+        user_id = self.object.user.pk
+        return reverse_lazy('view_profile', kwargs={'pk': user_id})
 
 
+@login_required(login_url='/login')
 class UpdateUserProfile(UpdateView):
     """view for the update user profile"""
 
     model = User
     form_class = UserUpdateForm
     template_name = 'update_profile.html'
-    success_url = reverse_lazy('index')
+
+    def get_success_url(self):
+        user_id = self.object.pk
+        return reverse_lazy('view_profile', kwargs={'pk': user_id})
 
 
-
+@login_required(login_url='/login')
 class Index(TemplateView):
     """view for the render index page """
     template_name = 'index.html'
 
 
+@login_required(login_url='/login')
 class ViewProfile(DetailView):
     """ To view user details """
     model = User
@@ -50,6 +58,7 @@ class ViewProfile(DetailView):
         return context
 
 
+@login_required(login_url='/login')
 class HomePageView(TemplateView):
     """ view for rendering index page"""
 
