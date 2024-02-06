59a07b5408ba13a64f65fe9c3a6f3b71816cce48
Yshah112 <yshah112002@gmail.com>
2024-02-06 10:45:40 +0530

    fixed signup form

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,9 @@
-from Scripts.bottle import url
+
 from django.contrib.auth.decorators import login_required
 from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
-from django.conf.urls import handler404
 
 urlpatterns = [
                   path('', include('allauth.urls')),
