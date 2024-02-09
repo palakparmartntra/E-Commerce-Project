85a2c11eb40adfbab2ed20b7110f825722cc26c7
Yshah112 <yshah112002@gmail.com>
2024-02-09 16:16:18 +0530

    added thorugh table into admin site

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Product, Category, Brand
+from .models import Product, Category, Brand,BrandProduct
 
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(Brand)
+admin.site.register(BrandProduct)
