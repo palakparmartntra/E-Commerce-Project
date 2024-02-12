2ccae347b358a47444f42595d488e2c604953bf2
Yshah112 <yshah112002@gmail.com>
2024-02-12 18:31:03 +0530

    added heading file

---
+++ products/headings.py
@@ -0,0 +1,6 @@
+
+
+class AdminPortalHeadings:
+    PRODUCT_ADDED = "Category Added Successfully"
+    PRODUCT_UPDATED = "Category Updated Successfully"
+    PRODUCT_DELETED = "Category Deleted Successfully"
