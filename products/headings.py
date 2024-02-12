7ebb300a01962ae5e436cfa6bcf397a8ed4486d7
Yshah112 <yshah112002@gmail.com>
2024-02-12 18:32:39 +0530

    changes in heading file

--- products/headings.py
+++ products/headings.py
@@ -1,6 +1,6 @@
 
 
 class AdminPortalHeadings:
-    PRODUCT_ADDED = "Product Added Successfully"
-    PRODUCT_UPDATED = "Product Updated Successfully"
-    PRODUCT_DELETED = "Product Deleted Successfully"
+    PRODUCT_ADDED = "Category Added Successfully"
+    PRODUCT_UPDATED = "Category Updated Successfully"
+    PRODUCT_DELETED = "Category Deleted Successfully"
