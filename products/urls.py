dc6386e3af8b9bedf50d4b0066b4e10a5f171c40
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 <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>
2024-02-12 12:30:03 +0530

    dashboard files added

---
+++ products/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from .views import home_page
+
+urlpatterns = [
+    path('', home_page, name='homepage'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- products/views.py
+++ products/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from .models import Brand, Category, Product
+from django.contrib import messages
+from django.db.models import Count
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required
+def home_page(request):
+    """" To redirect user to home page and superuser to dashboard """
+
+    context = {}
+    if not request.user.is_superuser:
+        return render(request, "index.html")
+
+    banner = Brand.objects.annotate(banner_count=Count("name")).all()
+    categories = Category.objects.annotate(catogory_count=Count("name")).all()
+    brands = Brand.objects.annotate(brands_count=Count("name")).all()
+    products = Product.objects.annotate(product_count=Count("name")).all()
+    heading = "Dashboard"
+
+    context = {
+        "heading": heading,
+        "banner_count": banner,
+        "categories_count": categories,
+        "brands_count": brands,
+        "products_count": products
+    }
+
+    return render(request, "product/dashboard.html", context)
