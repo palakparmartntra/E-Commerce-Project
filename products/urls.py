59c1be504a2f11790bbe77361a404e8b87a2c314
Yshah112 <yshah112002@gmail.com>
2024-02-12 18:34:16 +0530

    url formatting

--- products/urls.py
+++ products/urls.py
@@ -4,9 +4,8 @@ from django.conf import settings
 from .views import add_category, update_category, view_categroy, delete_category
 
 urlpatterns = [
-                  path('addcategory', add_category, name='add-category'),
-                  path('updatecategory/<int:pk>/', update_category, name='update-category'),
-                  path('viewcategory/', view_categroy, name='view-category'),
-                  path('deletecategory/<int:pk>/', delete_category, name='delete-category'),
-
-              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('addcategory', add_category, name='add-category'),
+    path('updatecategory/<int:pk>/', update_category, name='update-category'),
+    path('viewcategory/', view_categroy, name='view-category'),
+    path('deletecategory/<int:pk>/', delete_category, name='delete-category'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
