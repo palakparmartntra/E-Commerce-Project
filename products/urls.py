4f110ce27b4aa96622e0e6716e4d738f2ce3c9ea
Yshah112 <yshah112002@gmail.com>
2024-02-13 19:07:00 +0530

    [ADDED] user products page

--- products/urls.py
+++ products/urls.py
@@ -1,11 +1,13 @@
-
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from .views import category_view
-
+from .views import category_view, category_data,subcategory_data,product_data, all_products
 
 urlpatterns = [
     path('', category_view, name='user-category'),
+    path('category', category_data, name='category'),
+    path('subcategory/<int:pk>/', subcategory_data, name='subcategory'),
+    path('product/<int:pk>/', product_data, name='products'),
+    path('products/', all_products, name='all-products')
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- products/views.py
+++ products/views.py
@@ -1,15 +1,35 @@
-from django.shortcuts import render
-from .models import Category,Product
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
+
 
 # Create your views here.
 
 
 def category_view(request):
-    category = Category.objects.all()
+    category = Category.objects.filter(parent=None)
     product = Product.objects.all()
 
     return render(request, 'index.html', {'categorydata': category, 'productdata': product})
 
 
+def category_data(request):
+    category = Category.objects.filter(parent=None)
+    return render(request, 'user_product/category.html', {'categorydata': category})
 
 
+def subcategory_data(request, pk):
+    id_parent = get_object_or_404(Category, pk=pk)
+    print(id_parent)
+    subcategory = Category.objects.filter(parent=id_parent.pk)
+    return render(request, "user_product/subcategory.html", {'subcategorydata': subcategory})
+
+
+def product_data(request, pk):
+    id_parent = get_object_or_404(Category, pk=pk)
+    product = Product.objects.filter(category=id_parent.pk)
+    return render(request, 'user_product/products.html', {'productdata': product})
+
+
+def all_products(request):
+    product = Product.objects.all()
+    return render(request, 'user_product/all_product.html', {'productdata': product})
