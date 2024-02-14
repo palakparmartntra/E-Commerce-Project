7d687952c0a1d3286e916946f5e47d04c269f3b4
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 <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>
2024-02-14 18:31:17 +0530

    [ADDED] required fields

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,5 @@ from django.conf import settings
 from .views import product_detail
 
 urlpatterns = [
-    path('product/<int:pk>/', product_detail, name='product-detail')
+    path('product/<int:product_pk>/<int:brand_pk>/', product_detail, name='product-detail')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- products/views.py
+++ products/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
-from .models import Product, Category
+from .models import Product, Category, BrandProduct
 
 
-def product_detail(request, pk):
+def product_detail(request, product_pk, brand_pk):
     """ to show details of a product """
 
-    product = Product.objects.get(id=pk)
+    product = Product.objects.get(is_deleted=False, is_active=True, id=product_pk)
+    brand_of_product = BrandProduct.objects.get(product=product_pk, brand=brand_pk)
     categories = Category.objects.filter(parent=None)
 
     context = {
         "heading": product.name,
         "product": product,
-        "categories": categories
+        "categories": categories,
+        "brand_of_product": brand_of_product
     }
     return render(request, 'user_product/product_details.html', context)
