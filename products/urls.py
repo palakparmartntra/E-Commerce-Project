c1e81bb19ca4bc3c103b63ea4633b23355d142ce
Yshah112 <yshah112002@gmail.com>
2024-02-14 14:38:10 +0530

    [CHANGES] in url

--- products/urls.py
+++ products/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from .views import category_view, category_data,subcategory_data\
+from .views import category_data,subcategory_data\
     ,product_data, all_products, home_page
 
 
 urlpatterns = [
     path('', home_page, name='homepage'),
     path('profile/', home_page, name='admin-profile'),
-    path('', category_view, name='user-category'),
     path('category', category_data, name='category'),
     path('subcategory/<int:pk>/', subcategory_data, name='subcategory'),
     path('product/<int:pk>/', product_data, name='products'),
--- products/views.py
+++ products/views.py
@@ -6,40 +6,36 @@ from django.contrib.auth.decorators import login_required
 from .headings import AdminPortalHeadings
 
 
-@login_required
 def home_page(request):
     """" To redirect user to home page and superuser to dashboard """
 
     context = {}
     if not request.user.is_superuser:
-        return render(request, "index.html")
+        category = Category.objects.filter(parent=None)
+        if request.GET.get('search'):
+            category = category.filter(name__icontains=request.GET.get('search'))
+        product = Product.objects.all()
+        if request.GET.get('search'):
+            product = product.filter(name__icontains=request.GET.get('search'))
 
-    banner = Brand.objects.annotate(banner_count=Count("name")).all()
-    categories = Category.objects.annotate(catogory_count=Count("name")).all()
-    brands = Brand.objects.annotate(brands_count=Count("name")).all()
-    products = Product.objects.annotate(product_count=Count("name")).all()
-    heading = AdminPortalHeadings.DASHBOARD
+        return render(request, 'index.html', {'categorydata': category, 'productdata': product})
 
-    context = {
-        "heading": heading,
-        "banner_count": banner,
-        "categories_count": categories,
-        "brands_count": brands,
-        "products_count": products
-    }
+    else:
+        banner = Brand.objects.annotate(banner_count=Count("name")).all()
+        categories = Category.objects.annotate(catogory_count=Count("name")).all()
+        brands = Brand.objects.annotate(brands_count=Count("name")).all()
+        products = Product.objects.annotate(product_count=Count("name")).all()
+        heading = AdminPortalHeadings.DASHBOARD
 
-    return render(request, "product/dashboard.html", context)
+        context = {
+            "heading": heading,
+            "banner_count": banner,
+            "categories_count": categories,
+            "brands_count": brands,
+            "products_count": products
+        }
 
-
-def category_view(request):
-    category = Category.objects.filter(parent=None)
-    if request.GET.get('search'):
-        category = category.filter(name__icontains=request.GET.get('search'))
-    product = Product.objects.all()
-    if request.GET.get('search'):
-        product = product.filter(name__icontains=request.GET.get('search'))
-
-    return render(request, 'index.html', {'categorydata': category, 'productdata': product})
+        return render(request, "product/dashboard.html", context)
 
 
 def category_data(request):
