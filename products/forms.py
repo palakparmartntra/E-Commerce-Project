856029630b907ec5d99fe32d5e5a8e56f34dc611
Yshah112 <yshah112002@gmail.com>
2024-02-16 16:32:03 +0530

    changes in form

--- products/forms.py
+++ products/forms.py
@@ -6,3 +6,17 @@ class AddProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['name', 'description', 'quantity', 'price', 'image', 'category', 'is_active']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.TextInput(attrs={'class': 'form-control'}),
+            'quantity': forms.TextInput(attrs={'class': 'form-control'}),
+            'price': forms.TextInput(attrs={'class': 'form-control'}),
+            'image': forms.FileInput(attrs={'class': 'form-control'}),
+            'category': forms.TextInput(attrs={'class': 'form-control'}),
+
+
+
+
+
+
+        }
