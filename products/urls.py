f5bcd9d7ed719b259ccdf56436bb3760df493aed
Yshah112 <yshah112002@gmail.com>
2024-02-18 12:08:16 +0530

    addded media url and change add button position

--- products/urls.py
+++ products/urls.py
@@ -9,4 +9,4 @@ urlpatterns = [
     path('add-brand/', add_brand, name='add-brand'),
     path('update-brand/<int:pk>/', update_brands, name='update-brand'),
     path('delete-brand/<int:pk>/', delete_brand, name='delete-brand'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
