13a976a34dd726c3512b8914ccf920eda806ba28
Yshah112 <yshah112002@gmail.com>
2024-02-20 10:35:06 +0530

    fixed all changes of products

--- products/headings.py
+++ products/headings.py
@@ -3,9 +3,7 @@ class AdminPortalHeadings:
     PRODUCT_ADDED = "Product Added Successfully"
     PRODUCT_UPDATED = "Product Updated Successfully"
     PRODUCT_DELETED = "Product Deleted Successfully"
-    PRODUCT_ADDED = "Category Added Successfully"
-    PRODUCT_UPDATED = "Category Updated Successfully"
-    PRODUCT_DELETED = "Category Deleted Successfully"
+    PRODUCT_HEADING = 'All Products'
     CATEGORY_ADDED = "Category Added Successfully"
     CATEGORY_UPDATED = "Category Updated Successfully"
     CATEGORY_DELETED = "Category Deleted Successfully"
